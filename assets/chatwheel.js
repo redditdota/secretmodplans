b5381ac585011cf118eda998c77b069d0a6d2dd9
Sahil Dhanju <sahildhanju1@gmail.com>
2018-04-01 05:08:39 +0100

    Added audio buttons for other chatwheel files

--- assets/chatwheel.js
+++ assets/chatwheel.js
@@ -6,15 +6,29 @@ function setup(buttonId, audioId) {
 	    	this.innerHTML = "Play"
 	    	audio.pause();
 	    	audio.currentTime = 0
-	  	} else{
-	    	this.className = "is-playing";
-	    	this.innerHTML = "Pause";
-	    	audio.currentTime = 0
-	    	audio.play();
-	  	}
+	  	} else {
+			this.className = "is-playing";
+			this.innerHTML = "Pause";
+			that = this;
+			audio.currentTime = 0;
+			audio.onended = function() {
+				that.className = "";
+				that.innerHTML = "Play"
+				audio.currentTime = 0
+			};
+			audio.play();
+	}
 	});
 }
 
 setup("AudioButton1", "punks")
 setup("AudioButton2", "karma")
 setup("AudioButton3", "pleasemrlizard")
+setup("AudioButton4", "beta")
+setup("AudioButton12", "wp")
+setup("AudioButton6", "dreamcarl")
+setup("AudioButton7", "6treants")
+setup("AudioButton8", "simplecalculus")
+setup("AudioButton9", "thankyoupurge")
+setup("AudioButton10", "gg")
+setup("AudioButton11", "passiveaggressive")
